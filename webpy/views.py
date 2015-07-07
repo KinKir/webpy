ee3dcf485c2cb1225f5d8d66b6e4a8b02a364fb9
Evan Wilde <etcwilde@uvic.ca>
2015-07-07 16:30:39 -0700

    Adding Temporary Login Page

--- webpy/views.py
+++ webpy/views.py
@@ -1,6 +1,15 @@
 from webpy import app
 
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        # Check that user exists and log them in if they exist
+        return redirect(url_for('index'))
 
 
 @app.route('/', methods=['GET', 'POST'])
