786cccf1c04bc98b21382fc360c525d9abcfe953
SeanDebroni <sean.debroni@gmail.com>
2015-07-08 09:30:21 -0700

    Added Variable inputs/outputs to fields.

--- webpy/views.py
+++ webpy/views.py
@@ -3,4 +3,6 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    Input = "#Input your Python Program here"
+    Output = "Python Output Area"
+    return render_template('index.html', PyOutput = Output, PyInput=Input)
